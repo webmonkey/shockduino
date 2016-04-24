d52265dd5a812afb0cfda9219948638be9e12c75
James Cohen <james.cohen@gmail.com>
2016-04-24 17:28:23 +0100

    initial commit
    Hacked around version of code from https://www.sparkfun.com/tutorials/240
    Returns realtime data from all 3 axis

---
+++ shockduino.ino
@@ -0,0 +1,93 @@
+#include <SPI.h>
+
+//Assign the Chip Select signal to pin 6
+int CS=6;
+
+//This is a list of some of the registers available on the ADXL345.
+//To learn more about these and the rest of the registers on the ADXL345, read the datasheet!
+char POWER_CTL = 0x2D;	//Power Control Register
+char DATA_FORMAT = 0x31;
+char DATA_RATE = 0x2C;
+char DATAX0 = 0x32;	//X-Axis Data 0
+
+//This buffer will hold values read from the ADXL345 registers.
+char values[10];
+
+
+void setup(){ 
+  //Initiate an SPI communication instance.
+  SPI.begin();
+  //Configure the SPI connection for the ADXL345.
+  SPI.setDataMode(SPI_MODE3);
+  //Create a serial connection to display the data on the terminal.
+  Serial.begin(9600);
+  
+  //Set up the Chip Select pin to be an output from the Arduino.
+  pinMode(CS, OUTPUT);
+  //Before communication starts, the Chip Select pin needs to be set high.
+  digitalWrite(CS, HIGH);
+
+  // all from http://www.avrfreaks.net/forum/interfacing-adxl375-problem
+  writeRegister(DATA_FORMAT, 0x0b);
+  writeRegister(DATA_RATE, 0x0d);
+    
+  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
+  writeRegister(POWER_CTL, 0x08);  //Measurement mode  
+}
+
+void loop(){
+  //Reading 6 bytes of data starting at register DATAX0 will retrieve the x,y and z acceleration values
+  //The results of the read operation will get stored to the values[] buffer.
+  readRegister(DATAX0, 6, values);
+
+  short x = (((short)values[1]<<8)|(short)values[0]);
+  short y = (((short)values[3]<<8)|(short)values[2]);
+  short z = (((short)values[5]<<8)|(short)values[4]);
+
+  //Print the results to the terminal.
+  Serial.print(x, DEC);
+  Serial.print(',');
+  Serial.print(y, DEC);
+  Serial.print(',');
+  Serial.println(z, DEC);
+
+  delay(1000); 
+}
+
+//This function will write a value to a register on the ADXL345.
+//Parameters:
+//  char registerAddress - The register to write a value to
+//  char value - The value to be written to the specified register.
+void writeRegister(char registerAddress, char value){
+  //Set Chip Select pin low to signal the beginning of an SPI packet.
+  digitalWrite(CS, LOW);
+  //Transfer the register address over SPI.
+  SPI.transfer(registerAddress);
+  //Transfer the desired register value over SPI.
+  SPI.transfer(value);
+  //Set the Chip Select pin high to signal the end of an SPI packet.
+  digitalWrite(CS, HIGH);
+}
+
+//This function will read a certain number of registers starting from a specified address and store their values in a buffer.
+//Parameters:
+//  char registerAddress - The register addresse to start the read sequence from.
+//  int numBytes - The number of registers that should be read.
+//  char * values - A pointer to a buffer where the results of the operation should be stored.
+void readRegister(char registerAddress, int numBytes, char * values){
+  //Since we're performing a read operation, the most significant bit of the register address should be set.
+  char address = 0x80 | registerAddress;
+  //If we're doing a multi-byte read, bit 6 needs to be set as well.
+  if(numBytes > 1)address = address | 0x40;
+  
+  //Set the Chip select pin low to start an SPI packet.
+  digitalWrite(CS, LOW);
+  //Transfer the starting register address that needs to be read.
+  SPI.transfer(address);
+  //Continue to read registers until we've read the number specified, storing the results to the input buffer.
+  for(int i=0; i<numBytes; i++){
+    values[i] = SPI.transfer(0x00);
+  }
+  //Set the Chips Select pin high to end the SPI packet.
+  digitalWrite(CS, HIGH);
+}
