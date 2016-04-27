1a4bd53a2d993b793b5b1c04243722c54403ec97
James Cohen <james.cohen@gmail.com>
2016-04-27 22:33:22 +0100

    separate out the constructor/init, add debug and total size

--- AccelReading.cpp
+++ AccelReading.cpp
@@ -1,10 +1,30 @@
 #include "Arduino.h"
 #include "AccelReading.h"
 
-AccelReading::AccelReading(int16_t _x, int16_t _y, int16_t _z, uint8_t scalingFactor)
+AccelReading::AccelReading()
+{
+}
+
+void AccelReading::init(int16_t _x, int16_t _y, int16_t _z, uint8_t scalingFactor)
 {
   x = _x * scalingFactor;
   y = _y * scalingFactor;
   z = _z * scalingFactor;
+}
+
+int AccelReading::accelSize()
+{
+  return sqrt(sq(x) + sq(y) + sq(z));
+}
+
+void AccelReading::printDebug()
+{
+  Serial.print(x);
+  Serial.print("\t");
+  Serial.print(y);
+  Serial.print("\t");
+  Serial.print(z);
+  Serial.print("\t");
+  Serial.println(accelSize());
 }
 
--- AccelReading.h
+++ AccelReading.h
@@ -6,7 +6,10 @@
 class AccelReading
 {
   public:
-    AccelReading(int16_t x, int16_t y, int16_t z, uint8_t scalingFactor = 1);
+    AccelReading();
+    void init(int16_t x, int16_t y, int16_t z, uint8_t scalingFactor = 1);
+    int accelSize();
+    void printDebug();
     int x;
     int y;
     int z;
